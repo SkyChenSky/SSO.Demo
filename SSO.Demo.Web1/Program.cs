c2c13cb50671888a73d8f38971d34bc39218a71c
陈珙 <287245177@>
2017-09-18 22:18:55 +0800

    创建项目

---
+++ SSO.Demo.Web1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SSO.Demo.Web1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* var config = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("hosting.json", optional: true)
+                .Build();*/
+
+            BuildWebHost(args, null).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args, IConfigurationRoot config) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                 //.UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                 .UseIISIntegration()
+                 .UseStartup<Startup>()
+                 .UseApplicationInsights()
+                .Build();
+    }
+}
