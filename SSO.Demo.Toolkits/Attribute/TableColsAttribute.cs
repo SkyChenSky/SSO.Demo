c00e25abcdc8ecb501d9c5e1f07d8ce8fd18a62b
陈珙 <skychensky@outlook.com>
2017-10-19 14:23:53 +0800

    修改完毕11

---
+++ SSO.Demo.Toolkits/Attribute/TableColsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSO.Demo.Toolkits.Attribute
+{
+    public class TableColsAttribute : System.Attribute
+    {
+        public TableColsAttribute()
+        {
+            Width = 150;
+            Align = EAlign.Left;
+        }
+        public string Field { get; set; }
+
+        public string Tile { get; set; }
+
+        public int Width { get; set; }
+
+        public EAlign Align { get; set; }
+    }
+
+    public enum EAlign
+    {
+        [Display(Name = "center")]
+        Center = 0,
+        [Display(Name = "right")]
+        Right = 1,
+        [Display(Name = "left")]
+        Left = 2,
+    }
+}
