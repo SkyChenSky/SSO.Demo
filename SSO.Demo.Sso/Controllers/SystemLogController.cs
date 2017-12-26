d614e00f38407096225204863aa68ad16ad2cd00
陈珙 <skychensky@outlook.com>
2017-12-26 18:11:35 +0800

    系统日志完成

---
+++ SSO.Demo.Sso/Controllers/SystemLogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SSO.Demo.Sso.Instrumentation;
+
+namespace SSO.Demo.Sso.Controllers
+{
+    public class SystemLogController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
